0cd614f36cef24c998202dbacb8a412214afbaa3
Chandru1501 <chandraprakash001dpm@gmail.com>
2023-02-22 13:58:24 +0530

    my code

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("this is app.js")
+let headTitle = document.getElementById("header-title");
+headTitle.innerHTML="hello"
+headTitle.style.borderBottom="solid 5px black";
+headmain = document.getElementById("main-header");
+// headmain.style.border="solid 5px black";
+let additem =document.getElementById("title");
+additem.textContent="ADD ITEM";
+additem.style.color="green";
