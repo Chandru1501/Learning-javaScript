1749bc7d0a2832cc5776237e8c2d533d48503e3a
Chandru1501 <chandraprakash001dpm@gmail.com>
2023-03-01 23:17:38 +0530

    get coldDrinks with async

--- promise.js
+++ promise.js
@@ -1,46 +1,40 @@
 console.log("PERSON1: SHOWS TICKETS")
 console.log("PERSON2: SHOWS TICKETS")
 
+
+const newMovie = async () => {
+
 const promiseWifeBringTics = new Promise((resolve,reject) => {
       setTimeout(()=> {
-            resolve(` tickets `);
-      },3000)
+            resolve(` tickets `)  },3000)
 });
 
-const getPopcorn = promiseWifeBringTics.then((t)=>{
+const getPopcorn = new Promise((resolve,reject)=> resolve(`popcorn`));
+const addButter = new Promise((resolve,reject)=> resolve(`butter`));
+const getColdDrinks = new Promise((resolve,reject)=> resolve(`colddrinks`));
+
+const ticket = await promiseWifeBringTics
       console.log("here is the tickets");
       console.log("husband : we should go in");
       console.log("Wife : im very hungry can you bring me a popcorn please ? ");
 
-      return new Promise((resolve,reject) => {
-            resolve(`${t}popcorn `);
-      })
-})
-
-//getColdDrinks
-
-const getButter = getPopcorn.then((t)=> { 
+const popcorn = await getPopcorn;
       console.log("here is the popcorn");
       console.log("husband : we should go in");
       console.log("Wife : i want butter popcorn");
 
-      return new Promise ((resolve,reject)=> {
-            resolve(`${t}butterpopcorn`)
-      })
-})
-
-const getColdDrinks = getButter.then((t) => {
+const butter = await addButter;
       console.log(`husband : i got butter popcorn`);
       console.log("Wife : i want colddrinks");
 
-      return new Promise ((resolve,reject)=>{
-            resolve(`${t} coldDrinks ` )
-      })
-})
+const colddrinks = await getColdDrinks;
+      console.log(` here is th colddrink ` );
+      console.log(` lets go in ` );
 
-const goin = getColdDrinks.then((t)=> {
-      console.log(`${t} lets go in ` );
-})
+      return ticket;
+}
+
+newMovie().then((m) => console.log(`person 3 : shows ${m}`))
 
 
 
