eea6dcca3b32e30591184855203cae1d4e4c836f
Chandru1501 <chandraprakash001dpm@gmail.com>
2023-03-01 22:33:39 +0530

    getting colddrinks

--- promise.js
+++ promise.js
@@ -1,22 +1,48 @@
-const promise1 = Promise.resolve("Hello World");
-const promise2 = 10;
-const promise3 = new Promise((resolve,reject)=>
-      setTimeout(resolve,3000,`goodbye`)
-);
+console.log("PERSON1: SHOWS TICKETS")
+console.log("PERSON2: SHOWS TICKETS")
+
+const promiseWifeBringTics = new Promise((resolve,reject) => {
+      setTimeout(()=> {
+            resolve(` tickets `);
+      },3000)
+});
+
+const getPopcorn = promiseWifeBringTics.then((t)=>{
+      console.log("here is the tickets");
+      console.log("husband : we should go in");
+      console.log("Wife : im very hungry can you bring me a popcorn please ? ");
+
+      return new Promise((resolve,reject) => {
+            resolve(`${t}popcorn `);
+      })
+})
+
+//getColdDrinks
+
+const getButter = getPopcorn.then((t)=> { 
+      console.log("here is the popcorn");
+      console.log("husband : we should go in");
+      console.log("Wife : i want butter popcorn");
+
+      return new Promise ((resolve,reject)=> {
+            resolve(`${t}butterpopcorn`)
+      })
+})
+
+const getColdDrinks = getButter.then((t) => {
+      console.log(`husband : i got butter popcorn`);
+      console.log("Wife : i want colddrinks");
+
+      return new Promise ((resolve,reject)=>{
+            resolve(`${t} coldDrinks ` )
+      })
+})
+
+const goin = getColdDrinks.then((t)=> {
+      console.log(`${t} lets go in ` );
+})
 
 
-function userlastActivityTime(){
-    console.log(new Date);      
-}
 
-  setTimeout ( function () { console.log("before creating post 4, user lastActivityTime = ") + userlastActivityTime()},1000)
-
-Promise.all([promise1,promise2,promise3])
-.then(values => console.log(values))
-.then(
-      setTimeout (()=> {
-            console.log("after creating post 4, user lastActivityTime = "+new Date())
-      },5000)
-)
-
-
+console.log("PERSON4: SHOWS TICKETS")
+console.log("PERSON5: SHOWS TICKETS")
